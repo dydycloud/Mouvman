7bdaffdca248c046c404f5cdf791a1c12efbb947
dydycloud <sandy.pierre97@gmail.com>
2013-06-14 15:56:49 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mouvman::Application.config.secret_token = '84819b14bc3f246a5c5ad1e8fc515d3f53c69594415ac93f21a7d58c5488b3244f6b5b616cc3999a54d702b22bbc95632ca7fe7db4b83e5810c1bd4ba6e689ba'
